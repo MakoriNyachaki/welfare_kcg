5cf0072d7fea509a52932a54f8e33fd6c3f39699
makori <nyachakim@gmail.com>
2024-08-22 09:48:46 +0300

    Employees class

---
+++ employees.py
@@ -0,0 +1,6 @@
+'''A class used for capturing and registering all county law enforment officers
+employed under the county law enforcement department of Kisii County'''
+
+class Employees:
+	def __init__(self, emp_id, name, dob, emp_date, phone, email, address):
+		self.emp_id = emp_id
